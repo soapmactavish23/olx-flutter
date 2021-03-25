d3583d42720d4d556a3670da793d243301d7ffd0
HKprogram <ricknogueira1231@gmail.com>
2021-03-25 17:22:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "OLX",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
