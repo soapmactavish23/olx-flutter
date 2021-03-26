05d7ec2ac5b9dae045124c71b9061dbcacb2a9dc
HKprogram <ricknogueira1231@gmail.com>
2021-03-26 18:03:16 -0300

    Cadastro e login de usuário

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-
-import 'Home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:olx/view/Home.dart';
 
 final ThemeData temaPadrao = ThemeData(
   primaryColor: Color(0xff9c27b0),
   accentColor: Color(0xff7b1fa2)
 );
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     title: "OLX",
     home: Home(),
---
+++ lib/view/Home.dart
@@ -0,0 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:olx/models/Usuario.dart';
+
+import 'InputCustomizado.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+  bool _cadastrar = false;
+  String _msgErro = "";
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  String _textoBotao = "Entrar";
+
+  _cadastrarUsuario(Usuario usuario) {
+    _auth
+        .createUserWithEmailAndPassword(
+            email: usuario.email, password: usuario.senha)
+        .then((firebaseUser) {});
+  }
+
+  _logarUsuario(Usuario usuario) {
+    _auth
+        .signInWithEmailAndPassword(
+            email: usuario.email, password: usuario.senha)
+        .then((firebaseUser) {});
+  }
+
+  _validarCampos() {
+    //Recuperar dados dos campos
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if (email.isNotEmpty && email.contains("@")) {
+      if (senha.isNotEmpty && senha.length > 6) {
+        Usuario usuario = Usuario();
+        usuario.email = email;
+        usuario.senha = senha;
+
+        //Cadastrar ou logar
+        if (_cadastrar) {
+          _cadastrarUsuario(usuario);
+        } else {
+          _logarUsuario(usuario);
+        }
+      } else {
+        setState(() {
+          _msgErro = "Preencha a senha! digite mais de 6 caracteres";
+        });
+      }
+    } else {
+      setState(() {
+        _msgErro = "Prenncha o E-mail válido";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "imagens/logo.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                InputCustomizado(
+                  controller: _controllerEmail,
+                  hint: "E-mail",
+                  autofocus: true,
+                  type: TextInputType.emailAddress,
+                ),
+                InputCustomizado(
+                  controller: _controllerSenha,
+                  hint: "Senha",
+                  obscure: true,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Logar"),
+                    Switch(
+                      value: _cadastrar,
+                      onChanged: (bool valor) {
+                        setState(() {
+                          _cadastrar = valor;
+                        });
+                      },
+                    ),
+                    Text("Cadastrar")
+                  ],
+                ),
+                RaisedButton(
+                    child: Text(
+                      _cadastrar ? _textoBotao = "Cadastrar" : _textoBotao = "Entrar",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: Color(0xff9c27b0),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: (){
+                      _validarCampos();
+                    }
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    _msgErro,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
