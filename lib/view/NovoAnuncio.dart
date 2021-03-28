46b17be28d5274ac611a6b887a91a2ca3180199f
HKprogram <ricknogueira1231@gmail.com>
2021-03-28 13:03:41 -0300

    Dropdown e validação

--- lib/view/NovoAnuncio.dart
+++ lib/view/NovoAnuncio.dart
@@ -1,7 +1,10 @@
+import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:olx/view/widgets/BotaoCustomizado.dart';
 import 'dart:io';
+
+import 'package:validadores/Validador.dart';
 
 class NovoAnuncio extends StatefulWidget {
   @override
@@ -10,7 +13,12 @@ class NovoAnuncio extends StatefulWidget {
 
 class _NovoAnuncioState extends State<NovoAnuncio> {
   List<File> _listaImagens = List();
+  List<DropdownMenuItem<String>> _listaItensDropEstados = List();
+  List<DropdownMenuItem<String>> _listaItensDropCategorias = List();
   final _formKey = GlobalKey<FormState>();
+
+  String _itemSelecionadoEstado;
+  String _itemSelecionadoCategoria;
 
   _selecionarImagemGaleria() async {
     File imagemSelecionada;
@@ -20,13 +28,45 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
 
     imagemSelecionada = File(pickedFile.path);
 
-
-    if(imagemSelecionada != null){
+    if (imagemSelecionada != null) {
       setState(() {
         _listaImagens.add(imagemSelecionada);
       });
     }
+  }
 
+  @override
+  void initState() {
+    super.initState();
+    _carregarItensDropdown();
+  }
+
+  _carregarItensDropdown() {
+
+    //Categorias
+    _listaItensDropCategorias.add(
+      DropdownMenuItem(child: Text("Automóvel"), value: "auto",)
+    );
+
+    _listaItensDropCategorias.add(
+        DropdownMenuItem(child: Text("Imóvel"), value: "imovel",)
+    );
+
+    _listaItensDropCategorias.add(
+        DropdownMenuItem(child: Text("Eletrônico"), value: "eletro",)
+    );
+
+    _listaItensDropCategorias.add(
+        DropdownMenuItem(child: Text("Moda"), value: "moda",)
+    );
+
+    //Estados
+    for (var estado in Estados.listaEstadosSigla) {
+      _listaItensDropEstados.add(DropdownMenuItem(
+        child: Text(estado),
+        value: estado,
+      ));
+    }
   }
 
   @override
@@ -95,43 +135,52 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
 
                                 if (_listaImagens.length > 0) {
                                   return Padding(
-                                      padding: EdgeInsets.symmetric(horizontal: 8),
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          showDialog(
-                                              context: context,
-                                              builder: (context) => Dialog(
-                                                child: Column(
-                                                  mainAxisSize: MainAxisSize.min,
-                                                  children: [
-                                                  Image.file(_listaImagens[indice]),
-                                                  FlatButton(
-                                                    child: Text("Excluir"),
-                                                    textColor: Colors.red,
-                                                    onPressed: (){
-                                                      setState(() {
-                                                        _listaImagens.removeAt(indice);
-                                                        Navigator.of(context).pop();
-                                                      });
-                                                    },
-                                                  )
-                                                ],),
-                                              )
-                                          );
-                                        },
-                                        child: CircleAvatar(
-                                          radius: 50,
-                                          backgroundImage: FileImage(_listaImagens[indice]),
-                                          child: Container(
-                                            color: Color.fromRGBO(255, 255, 255, 0.4),
-                                            alignment: Alignment.center,
-                                            child: Icon(
-                                              Icons.delete,
-                                              color: Colors.red,
-                                            ),
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 8),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) => Dialog(
+                                                  child: Column(
+                                                    mainAxisSize:
+                                                        MainAxisSize.min,
+                                                    children: [
+                                                      Image.file(_listaImagens[
+                                                          indice]),
+                                                      FlatButton(
+                                                        child: Text("Excluir"),
+                                                        textColor: Colors.red,
+                                                        onPressed: () {
+                                                          setState(() {
+                                                            _listaImagens
+                                                                .removeAt(
+                                                                    indice);
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop();
+                                                          });
+                                                        },
+                                                      )
+                                                    ],
+                                                  ),
+                                                ));
+                                      },
+                                      child: CircleAvatar(
+                                        radius: 50,
+                                        backgroundImage:
+                                            FileImage(_listaImagens[indice]),
+                                        child: Container(
+                                          color: Color.fromRGBO(
+                                              255, 255, 255, 0.4),
+                                          alignment: Alignment.center,
+                                          child: Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
                                           ),
                                         ),
                                       ),
+                                    ),
                                   );
                                 }
 
@@ -149,7 +198,50 @@ class _NovoAnuncioState extends State<NovoAnuncio> {
                 ),
                 //MenusDropDown
                 Row(
-                  children: [Text("Estado"), Text("Categoria")],
+                  children: [
+                    Expanded(
+                        child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: DropdownButtonFormField(
+                        value: _itemSelecionadoEstado,
+                        hint: Text("Estados"),
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                        items: _listaItensDropEstados,
+                        validator: (valor) {
+                          return Validador()
+                              .add(Validar.OBRIGATORIO,
+                                  msg: "Campo Obrigatório")
+                              .valido(valor);
+                        },
+                        onChanged: (valor) {
+                          setState(() {
+                            _itemSelecionadoEstado = valor;
+                          });
+                        },
+                      ),
+                    )),
+                    Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: DropdownButtonFormField(
+                            value: _itemSelecionadoCategoria,
+                            hint: Text("Categorias"),
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                            items: _listaItensDropCategorias,
+                            validator: (valor) {
+                              return Validador()
+                                  .add(Validar.OBRIGATORIO,
+                                  msg: "Campo Obrigatório")
+                                  .valido(valor);
+                            },
+                            onChanged: (valor) {
+                              setState(() {
+                                _itemSelecionadoCategoria = valor;
+                              });
+                            },
+                          ),
+                        )),
+                  ],
                 ),
                 //Caixas de textos e botoes
                 Text("Caixas de textos"),
